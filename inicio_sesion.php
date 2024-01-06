14d37a63d1c0fe01d0cac70823e5d2205c5c3bfb
Yener <yenerandres22@gmail.com>
2024-01-06 14:21:07 -0500

    bootstrap

--- inicio_sesion.php
+++ inicio_sesion.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Palm Oil</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/inicio-sesion.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 </head>
 <body>
 
@@ -22,15 +22,17 @@
     <div>
         <img src="IMAGENES/logo.png" class="logo" alt="fondo">
     </div>
+
+    <h2 class="inicio">Iniciar Sesion</h2>
+
     <div class="contenedor">
-        <h2 class="subti">Iniciar Sesion</h2>
+        <form class="formulario" action="" method= "POST">
         <svg class="icono-usuario" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
           </svg>
           <svg class="icono-contraseña" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
             <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
           </svg>
-        <form action="" method= "POST">
             <input type="text" name="usuario" class="nombre" placeholder="Usuario"><br>
             <input type="password" name="contraseña" class="contraseña" placeholder="contraseña"><br>
             <input type="submit" name="ingresar" class="bot" id="boton" placeholder="ingresar">
@@ -51,4 +53,8 @@
 
     <div class="comprobacion"></div>
 </body>
+    <script src="sweetalert/dist/sweetalert2.all.js"></script>
+    <script src="sweetalert/dist/sweetalert2.all.min.js"></script>
+    <script src="sweetalert/jquery-3.6.0.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
 </html>

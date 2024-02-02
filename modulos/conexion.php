3517974409e2fda9152ea8defe22d4c8c4916a76
Yener <yenerarismendi52@gmail.com>
2024-02-01 20:37:08 -0500

    compartir

--- modulos/conexion.php
+++ modulos/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexionsql=new mysqli("localhost", "root", "","palmoil","3306");
+$conexionsql=new mysqli("localhost", "root", "","palmoil","3310");
 $conexionsql->set_charset("utf8");
 
 ?>

4b2840234f0152be195f509d2bc4708e61c8fcff
alejandro11b <145237267+alejandro11b@users.noreply.github.com>
2024-04-11 05:49:44 -0500

    Add files via upload

--- estadistica.php
+++ estadistica.php
@@ -123,40 +123,7 @@ echo "</table>";
 
 <!-- Scripts JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-    function eliminarRegistro(id) {
-        if (confirm('¿Estás seguro de que quieres eliminar este registro?')) {
-            $.ajax({
-                type: 'POST',
-                url: 'eliminar_registro.php', // Ruta a tu script PHP para eliminar registros
-                data: { id: id },
-                success: function(response) {
-                    if (response === 'success') {
-                        $('#fila-' + id).remove(); // Eliminar la fila de la tabla
-                    } else {
-                        alert('Error al eliminar el registro');
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.error(xhr.responseText);
-                    alert('Error al eliminar el registro');
-                }
-            });
-        }
-    }
 
-    function abrirModalEdicion(id) {
-        // Aquí puedes implementar la lógica para cargar los datos del producto en el formulario dentro del modal
-        // Por ejemplo, puedes usar AJAX para obtener los datos del producto desde la base de datos y completar los campos del formulario
-        // Luego, muestras el modal de edición
-        $('#modalEditar').css('display', 'block');
-    }
-
-    function cerrarModalEdicion() {
-        $('#modalEditar').css('display', 'none');
-    }
-
-</script>
 
 
 
@@ -267,7 +234,7 @@ echo "</table>";
 -->
 
 
-<script src="JS/movimiento-y-estadistica.js"></script>
+<script src="../JS/estadistica.js"></script>
 
 
 
